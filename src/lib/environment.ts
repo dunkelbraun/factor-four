8de62c484252c3bc9c5afd4b418989d0429c2908
Marcos Essindi <marcessindi@me.com>
2024-09-14 14:40:50 +0200

    Remove file

--- src/lib/environment.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the current mono environment from the `MONO_ENV` environment variable.
- *
- * @default local
- */
-export function monoEnv() {
-	return process.env.MONO_ENV || "local";
-}
