a7135a032599b20b0f2d7d40b68690251059f7af
Marcos Essindi <marcessindi@me.com>
2024-09-14 15:53:07 +0200

    Setup docs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+export { type MemcachedContainer } from "./key-value-stores/memcached/container.js";
 export {
 	defineMemcachedStore,
 	type MemcachedStore,
 } from "./key-value-stores/memcached/store.js";
+export { type RedisContainer } from "./key-value-stores/redis/container.js";
 export {
 	defineRedisStore,
 	type RedisStore,
 } from "./key-value-stores/redis/store.js";
+export {
+	type SESContainer,
+	type SESContainerInfo,
+} from "./mailers/ses/container.js";
 export { defineSESMailer, type SESMailer } from "./mailers/ses/ses.js";
+export {
+	type SMTPContainer,
+	type SMTPContainerInfo,
+} from "./mailers/smtp/container.js";
 export { defineSMTPMailer, type SMTPMailer } from "./mailers/smtp/smtp.js";
